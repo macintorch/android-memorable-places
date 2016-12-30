9260234eacf575fa52988c614f3ff4947ae468ba
Ainor Syahrizal <macintorch@gmail.com>
2016-12-30 09:30:49 +0800

    add array list to list view and google map setup

--- app/src/main/java/ainor/com/my/memorableplaces/MainActivity.java
+++ app/src/main/java/ainor/com/my/memorableplaces/MainActivity.java
@@ -2,6 +2,13 @@ package ainor.com.my.memorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView placesListView = (ListView) findViewById(R.id.placesListView);
+
+        ArrayList<String> places = new ArrayList<>();
+
+                places.add("Add a new place..");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,places);
+
+        placesListView.setAdapter(arrayAdapter);
+
+        placesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, i, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
