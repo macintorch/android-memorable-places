2c9f7865972acdfdbe485bddd22e91f5df713264
Ainor Syahrizal <macintorch@gmail.com>
2016-12-30 09:54:40 +0800

    get user current location

--- app/src/main/java/ainor/com/my/memorableplaces/MapsActivity.java
+++ app/src/main/java/ainor/com/my/memorableplaces/MapsActivity.java
@@ -1,8 +1,14 @@
 package ainor.com.my.memorableplaces;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,6 +21,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void centerMapOnLocation (Location location, String title) {
+        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+
+        mMap.clear();
+
+        mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,10));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +60,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         Intent intent = getIntent();
 
-        Toast.makeText(this, intent.getStringExtra("placeNumber"), Toast.LENGTH_SHORT).show();
+        if (intent.getIntExtra("placeNumber", 0) == 0) {
+
+            //zoom in user's location
+
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    centerMapOnLocation(location, "Your location");
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+
+
+        }
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
